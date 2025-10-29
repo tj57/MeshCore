edb98d32c21d63aec07d5753563bdedb810b881c
Scott Powell <sqij@protonmail.com>
2025-10-30 00:03:10 +1100

    * direct.txdelay default now 0.2 (was zero)

--- examples/simple_room_server/MyMesh.cpp
+++ examples/simple_room_server/MyMesh.cpp
@@ -593,6 +593,7 @@ MyMesh::MyMesh(mesh::MainBoard &board, mesh::Radio &radio, mesh::MillisecondCloc
   _prefs.airtime_factor = 1.0;   // one half
   _prefs.rx_delay_base = 0.0f;   // off by default, was 10.0
   _prefs.tx_delay_factor = 0.5f; // was 0.25f;
+  _prefs.direct_tx_delay_factor = 0.2f; // was zero
   StrHelper::strncpy(_prefs.node_name, ADVERT_NAME, sizeof(_prefs.node_name));
   _prefs.node_lat = ADVERT_LAT;
   _prefs.node_lon = ADVERT_LON;
