9c4e2d10706abcdb1a520844d32b2174871ff4cb
Frieder Schrempf <frieder@fris.de>
2025-12-19 11:39:29 +0100

    variants: Lilygo T-Echo (Lite): Use DC/DC regulator
    The schematic shows the LC circuit for the internal DC/DC regulator
    to be available. Enable it to save power.
    
    Signed-off-by: Frieder Schrempf <frieder@fris.de>

--- variants/lilygo_techo/TechoBoard.h
+++ variants/lilygo_techo/TechoBoard.h
@@ -13,7 +13,7 @@
 #define PIN_VBAT_READ     (4)
 #define REAL_VBAT_MV_PER_LSB (VBAT_DIVIDER_COMP * VBAT_MV_PER_LSB)
 
-class TechoBoard : public NRF52BoardOTA {
+class TechoBoard : public NRF52BoardDCDC, public NRF52BoardOTA {
 public:
   TechoBoard() : NRF52BoardOTA("TECHO_OTA") {}
   void begin();
