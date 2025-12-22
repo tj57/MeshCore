306428df03d234d79c4a673eaee1f8647904b150
Frieder Schrempf <frieder@fris.de>
2025-12-22 16:21:44 +0100

    variants: Nano G2 Ultra: Use common implementation of startOTAUpdate()
    Signed-off-by: Frieder Schrempf <frieder@fris.de>

--- variants/nano_g2_ultra/nano-g2.h
+++ variants/nano_g2_ultra/nano-g2.h
@@ -35,11 +35,11 @@
 #define PIN_VBAT_READ            (0 + 2)
 #define REAL_VBAT_MV_PER_LSB     (VBAT_DIVIDER_COMP * VBAT_MV_PER_LSB)
 
-class NanoG2Ultra : public NRF52Board {
+class NanoG2Ultra : public NRF52BoardOTA {
 public:
+  NanoG2Ultra() : NRF52BoardOTA("NANO_G2_OTA") {}
   void begin();
   uint16_t getBattMilliVolts() override;
-  bool startOTAUpdate(const char *id, char reply[]) override;
 
   const char *getManufacturerName() const override { return "Nano G2 Ultra"; }
 
