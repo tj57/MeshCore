91fbc40b0e4dfc80a53bb2908f03a8397975cfef
liamcottle <liam@liamcottle.com>
2026-01-26 23:05:10 +1300

    increase maximum flood advert interval to 168 hours (7 days)

--- src/helpers/CommonCLI.cpp
+++ src/helpers/CommonCLI.cpp
@@ -422,8 +422,8 @@ void CommonCLI::handleCommand(uint32_t sender_timestamp, const char* command, ch
         strcpy(reply, "OK");
       } else if (memcmp(config, "flood.advert.interval ", 22) == 0) {
         int hours = _atoi(&config[22]);
-        if ((hours > 0 && hours < 3) || (hours > 48)) {
-          strcpy(reply, "Error: interval range is 3-48 hours");
+        if ((hours > 0 && hours < 3) || (hours > 168)) {
+          strcpy(reply, "Error: interval range is 3-168 hours");
         } else {
           _prefs->flood_advert_interval = (uint8_t)(hours);
           _callbacks->updateFloodAdvertTimer();
